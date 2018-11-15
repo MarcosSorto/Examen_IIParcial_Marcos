7ad2d545887ad59b62a09f39a646a3e140246ddc
marcoselisorto <marcoselisorto@hotmail.com>
2018-11-15 10:25:56 -0600

    Creacion de la rama develop y diseño del formulario principal

--- frmPrincipal/frmPrincipal/Form1.Designer.cs
+++ frmPrincipal/frmPrincipal/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace frmPrincipal
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(77, 22);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(171, 24);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "BulletProofsRecord";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(238, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(320, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "opciones:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Reportes de Canciones";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(122, 25);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(77, 51);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar Canciones";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(13, 25);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(77, 51);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar Canciones";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(349, 217);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BulletProofRecords";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
 
--- frmPrincipal/frmPrincipal/Form1.cs
+++ frmPrincipal/frmPrincipal/Form1.cs
@@ -10,11 +10,27 @@ using System.Windows.Forms;
 
 namespace frmPrincipal
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Cierra el formulario proncipal y cierra la aplicación
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult resp = MessageBox.Show("¿Está seguro que desea salir?", "Control de canciones", MessageBoxButtons.YesNo);
+
+            //Verificamos la respuesta del usuario
+            if (resp== DialogResult.Yes)
+            {
+                this.Close();
+            }
         }
     }
 }
